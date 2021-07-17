2368b585cde08c7b525ea992795a2d956d5c327b
Martynas Kazlauskas <martynas@firmfirm.co>
2021-07-17 19:31:53 +0300

    chore: attempt to fix ci issues

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 // jest-puppeteer.config.js
 module.exports = {
   server: {
-    command: `yarn start`,
+    command: `yarn start --no-watch`,
     port: 3000,
     launchTimeout: 20000,
     debug: true,
